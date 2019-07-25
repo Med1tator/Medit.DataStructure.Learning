b1847937e33bbbfcb20f01d939e942ef3c2fd272
Medit <med1tator@outlook.com>
2019-07-25 23:54:53 +0800

    commits.

---
+++ 003.ArrayStack/ArrayStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _003.ArrayStack
+{
+    public class ArrayStack<T>
+    {
+        private T[] _array;
+        private int _size;
+
+        public ArrayStack()
+        {
+            _array = new T[10];
+            _size = -1;
+        }
+
+        public void Push(T value)
+        {
+            if (_size == _array.Length - 1)
+            {
+                var newArray = new T[_array.Length * 2];
+                _array.CopyTo(newArray, 0);
+                _array = newArray;
+            }
+
+            _array[++_size] = value;
+        }
+
+        /// <summary>
+        /// Returns the object at the top of the Stack without removing it. 
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            if (_size < 0)
+                throw new IndexOutOfRangeException();
+
+            return _array[_size];
+        }
+
+        /// <summary>
+        /// Removes and returns the object at the top of the Stack.
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            if (_size < 0)
+                throw new IndexOutOfRangeException();
+
+            return _array[_size--];
+        }
+    }
+}
---
+++ 003.ArrayStack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _003.ArrayStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var s = new Stack<int>();
+            s.Pop();
+            var stack = new ArrayStack<int>();
+            for (int i = 0; i < 25; i++)
+            {
+                stack.Push(i);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(stack.Peek());
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
